325f38f113b99a643a6df930ab993e8da4c815b8
George Higbie <gehigbie@gmail.com>
2018-05-01 15:35:13 +0000

    Add shells for components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyReadsApp from './MyReadsApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyReadsApp />, document.getElementById('app-root'));
 registerServiceWorker();
