b393795aa8ad0cbf7b1ab94d769e4a4389d77077
George Higbie <gehigbie@gmail.com>
2018-05-01 16:02:42 +0000

    Import components into Reads section component

--- src/Components/ReadsSection/ReadsSection.js
+++ src/Components/ReadsSection/ReadsSection.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import CurrentlyReading from './../CurrentlyReading';
+import WantToRead from './../WantToRead';
+import Read from './../Read';
+
 const ReadsSection = (props) => {
     return(
         <div>
             <h2>Reads Section</h2>
+            <CurrentlyReading />
+            <WantToRead />
+            <Read />
         </div>
     );
 };
