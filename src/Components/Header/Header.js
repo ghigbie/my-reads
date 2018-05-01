0e2789a4b13d080a5dbfb16e0aa8384a9349e364
George Higbie <gehigbie@gmail.com>
2018-05-01 17:16:10 +0000

    Remove boilerplate jsx

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Header.css';
 
 const Header = (props) => {
     return(
-        <div>
-            <h2>{props.title}</h2>
-            <h4>{props.subtitle}</h4>
-        </div>
+        <nav className="navbar navbar-inverse"
+             id="header-background">
+            <div className="container">
+                <div className="row extra-margin">
+                    <div className="col-sm-8">
+                        <h2 id="title">{props.title}</h2>
+                        <span><h4 id="subtitle">{props.subtitle}</h4></span>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
