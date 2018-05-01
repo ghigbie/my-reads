72e7f293bde36f33ca05b822199d3dae48835bac
George Higbie <gehigbie@gmail.com>
2018-05-01 15:39:45 +0000

    Create basic CurrentlyReading component

---
+++ src/Components/CurrentlyReading/CurrentlyReading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CurrentlyReading extends Component{
+    render(){
+        return(
+            <div>
+                <h3>CurrentlyReading</h3>
+            </div>
+        );
+    }
+}
+
+export default CurrentlyReading;
