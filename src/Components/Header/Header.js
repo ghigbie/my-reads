0e0df8e5af0b467f8d32d282c082ea199d86def7
George Higbie <gehigbie@gmail.com>
2018-05-01 17:28:14 +0000

    Working on styles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,17 +3,12 @@ import './Header.css';
 
 const Header = (props) => {
     return(
-        <nav className="navbar navbar-inverse"
-             id="header-background">
+        <div id="header-background">
             <div className="container">
-                <div className="row extra-margin">
-                    <div className="col-sm-8">
-                        <h2 id="title">{props.title}</h2>
-                        <span><h4 id="subtitle">{props.subtitle}</h4></span>
-                    </div>
-                </div>
+                <h2 id="title">{props.title}</h2>
+                <h4 id="subtitle">{props.subtitle}</h4>
             </div>
-        </nav>
+        </div>
     );
 };
 
