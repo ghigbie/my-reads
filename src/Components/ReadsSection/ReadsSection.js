b213960b0f4a3752c872e67d41ca70d196aef2e5
George Higbie <gehigbie@gmail.com>
2018-05-01 15:42:02 +0000

    Add ReadsSection component

---
+++ src/Components/ReadsSection/ReadsSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReadsSection = (props) => {
+    return(
+        <div>
+            <h2>Reads Section</h2>
+        </div>
+    );
+};
+
+export default ReadsSection;
