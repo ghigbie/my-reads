ca0c5d24532335fd4d28bbf82a14301aa73fb152
George Higbie <gehigbie@gmail.com>
2018-05-01 21:37:15 +0000

    Add new sections

--- src/MyReadsApp.js
+++ src/MyReadsApp.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './MyReadsApp.css';
 
 import Header from './Components/Header/Header';
+import NewReadsSection from './Components/NewReadsSection/NewReadsSection';
 import ReadsSection from './Components/ReadsSection/ReadsSection';
 import Footer from './Components/Footer/Footer';
 
@@ -15,13 +16,15 @@ class MyReadsApp extends Component {
   title = 'MyReads';
   subtitle = 'The world\'s most okayist book tracker...';
   footerText = 'The best way to organize your reading endevors...';
+  sectionTitles = ['Currently Reading', 'Want To Read', 'Read'];
   
   render() {
     return (
         <div>
           <Header title={this.title}
                   subtitle={this.subtitle}/>
-          <ReadsSection className="container" />
+          <ReadsSection className="container"
+                        sectionTitles={this.sectionTitles}/>
           <Footer footerText={this.footerText}/>
       </div>
     );
