6f63efa67ca8c82bf27286f7d03b668e6bffdb49
George Higbie <gehigbie@gmail.com>
2018-05-01 15:47:39 +0000

    Add Read component

---
+++ src/Components/Read/Read.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Read extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Read</h3>
+            </div>
+        );
+    }
+}
+
+export default Read;
