fac5b053231833db79b334057c73482a5a6332b3
George Higbie <gehigbie@gmail.com>
2018-05-01 16:55:42 +0000

    Add jquery to public

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,12 +3,10 @@ import './Footer.css'
 
 const Footer = () => {
     return(
-        <div className>
-             <div className="container">
+        <div className="container">
                 <div className="container-fluid">
                    The best way to organize your reading endevors...
                 </div>
-            </div>
         </div>
     );
 };
