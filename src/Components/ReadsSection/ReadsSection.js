04c0d0c5448e4a9fcb2ecfa991f122760ec5716c
George Higbie <gehigbie@gmail.com>
2018-05-01 16:28:05 +0000

    Edit import errors

--- src/Components/ReadsSection/ReadsSection.js
+++ src/Components/ReadsSection/ReadsSection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import CurrentlyReading from './../CurrentlyReading';
-import WantToRead from './../WantToRead';
-import Read from './../Read';
+import CurrentlyReading from './../CurrentlyReading/CurrentlyReading';
+import WantToRead from './../WantToRead/WantToRead';
+import Read from './../Read/Read';
 
 const ReadsSection = (props) => {
     return(
