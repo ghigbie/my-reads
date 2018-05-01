320ed973d91185106c38be93e8c06a2c73f006bd
George Higbie <gehigbie@gmail.com>
2018-05-01 15:44:43 +0000

    Add Want to Read component

---
+++ src/Components/WantToRead/WantToRead.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WantToRead extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Want to Read</h3>
+            </div>
+        );
+    }
+}
+
+export default WantToRead;
