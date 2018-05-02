f3b8e2e9c7530675f44ef546b471dbcddc4ee2aa
George Higbie <gehigbie@gmail.com>
2018-05-02 14:46:47 +0000

    Change header to have an implicit return

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Header.css';
 
-const Header = (props) => {
-    return(
-        <div id="header-background">
-            <div className="container">
-                <h3 id="title">{props.title}</h3>
-                <h5 id="subtitle">{props.subtitle}</h5>
-            </div>
+const Header = (props) => (
+    <div id="header-background">
+        <div className="container">
+            <h3 id="title">{props.title}</h3>
+            <h5 id="subtitle">{props.subtitle}</h5>
         </div>
-    );
-};
+    </div>
+);
 
 export default Header;
