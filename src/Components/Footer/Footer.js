b23e0af4d6ceba2d397474b122d2d36fd6b3a213
George Higbie <gehigbie@gmail.com>
2018-05-02 14:47:56 +0000

    Change footer to have an implicit return

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
-    return(
-        <div className="footer">
-                <div className="container-fluid">
-                   <h5>The best way to organize your reading endevors...</h5>
-                </div>
+const Footer = () => (
+    <div className="footer">
+        <div className="container-fluid">
+            <h5>The best way to organize your reading endevors...</h5>
         </div>
-    );
-};
+    </div>
+);
 
 export default Footer;
