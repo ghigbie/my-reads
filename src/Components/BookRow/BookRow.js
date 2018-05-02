ff91ed02021a39a137f278a7d26415fcd8a695c7
George Higbie <gehigbie@gmail.com>
2018-05-02 02:04:03 +0000

    Edit styles on boorow component

--- src/Components/BookRow/BookRow.js
+++ src/Components/BookRow/BookRow.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './BookRow.css';
 
 class BookRow extends Component{
     render(){
         return(
             <div>
-                <h4>{this.props.title}</h4>
+                <h4 className="book-title">{this.props.title}</h4>
                 <hr />
                 <div className="books-container">
                 
