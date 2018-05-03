3d595fe8ecc5b88474d9e0ed9c5143a42c70521b
George Higbie <gehigbie@gmail.com>
2018-05-03 15:48:57 +0000

    Add substance to cards

--- src/Components/BookItem/BookItem.js
+++ src/Components/BookItem/BookItem.js
@@ -5,10 +5,11 @@ import PropTypes from 'prop-types';
 const BookItem = (props) => {
     return(
         <div className="card book-item" style={{width: '18rem'}}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
+            <img className="card-img-top" src={props.image} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h3>{props.author}</h3>
+                <p className="card-text">{props.description}</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
