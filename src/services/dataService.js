2dd7bc4246c44b4d30c0acbb738b52df1ef6f852
George Higbie <gehigbie@gmail.com>
2018-05-04 15:56:52 +0000

    Remove dataService file

--- src/services/dataService.js
+++
@@ -1,7 +0,0 @@
- export const title = 'MyReads';
- export const subtitle = 'The world\'s most okayist book tracker...';
- export const footerText = 'The best way to organize your reading endevors...';
- export const sectionTitles = [{shelfCategory: 'currentlyReading', heading: 'Currently Reading'},
-                               {shelfCategory: 'wantToRead', heading: 'Want to Read'},
-                               {shelfCategory: 'read', heading: 'Read'}];
-  
