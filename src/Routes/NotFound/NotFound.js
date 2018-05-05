717e2b87acbf9c9dda5a179af874ba96a380078c
George Higbie <gehigbie@gmail.com>
2018-05-05 02:48:39 +0000

    Add implicit return statement

--- src/Routes/NotFound/NotFound.js
+++ src/Routes/NotFound/NotFound.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const NotFound = (props) => {
-    return(
-        <div className="container">
-            <h1>This page could not be found</h1>
-            <h2>What are you doing with your life?</h2>
-        </div>
-    );
-};
+const NotFound = (props) => (
+    <div className="container">
+        <h1>This page could not be found</h1>
+        <h2>What are you doing with your life?</h2>
+    </div>
+);
+
 
 export default NotFound;
