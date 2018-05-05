73c758dc19d44f48d1d574d8e561ca0f8d7fb95d
George Higbie <gehigbie@gmail.com>
2018-05-05 04:01:11 +0000

    Add route styles file

--- src/Routes/AddBook/AddBook.js
+++ src/Routes/AddBook/AddBook.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './../routeStyles.css';
 
 const AddBook = () => (
     <div>
+        <NavLink to="/"></NavLink>
         Add Book Page
     </div>
 );
