d03f467902d1ad1e94d444182fcd9f49c59cc885
George Higbie <gehigbie@gmail.com>
2018-05-05 22:25:04 +0000

    Edit import paths

--- src/Components/AddBookForm/AddBookForm.js
+++ src/Components/AddBookForm/AddBookForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './AddBook.css';
+import './AddBookForm.css';
 
 const AddBookForm = () => (
     <form>
--- src/Routes/AddBook/AddBook.js
+++ src/Routes/AddBook/AddBook.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import BackArrow from './../../Components/BackArrow/BackArrow';
+import AddBookForm from './../../Components/AddBookForm/AddBookForm';
 
 const AddBook = () => (
     <div>
         <BackArrow />
+        <AddBookForm />
         Add Book Page
     </div>
 );
