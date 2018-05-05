6cceb772c9b1899ace34fde37284693f7706f5f1
George Higbie <gehigbie@gmail.com>
2018-05-05 03:03:21 +0000

    Modify compnent name

--- src/Routes/BookInfo/BookInfo.js
+++ src/Routes/BookInfo/BookInfo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const IndividualBook = (props) => (
+const BookInfo = (props) => (
     <div className="container">
         <h1>More information about a particular book</h1>
     </div>
 );
 
 
-export default IndividualBook;
+export default BookInfo;
