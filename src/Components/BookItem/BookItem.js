cece72dde7252e79c4bac91b7e12b55e735c0ad7
George Higbie <gehigbie@gmail.com>
2018-05-05 02:50:45 +0000

    Add implicit return statement

--- src/Components/BookItem/BookItem.js
+++ src/Components/BookItem/BookItem.js
@@ -2,22 +2,19 @@ import React from 'react';
 import './BookItem.css';
 import PropTypes from 'prop-types';
 
-const BookItem = (props) => {
-    
-    return(
-        <div className="card book-item">
-            <img className="card-img-top book-image" src={props.image} alt={props.title}/>
-            <div className="card-body">
-                <h5 className="card-title book-title">{props.title}</h5>
-                <h5 className="card-title by-line">{`By ${props.authors}`}</h5>
-                <p className="card-text book-description">{`${props.description.substring(0, 188)}...`}</p>
-                <button className="book-shelf-changer"></button>
-                <a href="#" 
-                   className="btn btn-outline-primary btn-bottom">More Information</a>
-            </div>
+const BookItem = (props) => (
+    <div className="card book-item">
+        <img className="card-img-top book-image" src={props.image} alt={props.title}/>
+        <div className="card-body">
+            <h5 className="card-title book-title">{props.title}</h5>
+            <h5 className="card-title">{`By ${props.authors}`}</h5>
+            <p className="card-text book-description">{`${props.description.substring(0, 188)}...`}</p>
+            <a href="#" 
+               className="btn btn-outline-primary btn-bottom">More Information</a>
         </div>
-    );
-};
+    </div>
+);
+
 
 BookItem.propTypes = {
     title: PropTypes.string.isRequired,
