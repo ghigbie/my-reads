bee6943b597c61cb56cec2e5e9c5d0725fcc9c44
George Higbie <gehigbie@gmail.com>
2018-05-05 22:51:59 +0000

    Creact function for form

--- src/Components/AddBookForm/AddBookForm.js
+++ src/Components/AddBookForm/AddBookForm.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import serializeForm from 'form-serialize';
 import './AddBookForm.css';
 
-const AddBookForm = () => (
-    <div>
-    <form>
-        A form is here
-    </form>
-    </div>
-);
+class AddBookForm extends Component{
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        const values = serializeForm(e.target, {hash: true});
+        console.log('Values: ', values);
+    }
+    
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="title" placeholder="Title" />
+                    <input type="text" name="authors" placeholder="Author(s)" />
+                    <input type="textarea" name="description" placeholder="Description"/>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default AddBookForm;
