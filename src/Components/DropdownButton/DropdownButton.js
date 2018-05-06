190d8784bd528e10fc94204a8ff655230b3d7438
George Higbie <gehigbie@gmail.com>
2018-05-06 21:07:37 +0000

    Reorganize component

--- src/Components/DropdownButton/DropdownButton.js
+++ src/Components/DropdownButton/DropdownButton.js
@@ -2,24 +2,24 @@ import React from 'react';
 import './DropdownButton.css';
 import PropTypes from 'prop-types';
 
-const DropdownButton = () => (
-            <div className="dropdown">
-                <button className="book-shelf-changer" 
-                        type="button" 
-                        id={props.id}
-                        data-toggle="dropdown" 
-                        aria-haspopup="true">
-                </button>
-                <div className="dropdown-menu" 
-                     aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" 
-                       href="#">Action</a>
-                    <a className="dropdown-item" 
-                       href="#">Another action</a>
-                    <a className="dropdown-item"
-                       href="#">Something else here</a>
-                </div>
-            </div>
+const DropdownButton = (props) => (
+    <div className="dropdown">
+        <button className="book-shelf-changer" 
+                type="button" 
+                id={props.id}
+                data-toggle="dropdown" 
+                aria-haspopup="true">
+        </button>
+        <div className="dropdown-menu" 
+             aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" 
+               href="#">Action</a>
+            <a className="dropdown-item" 
+               href="#">Another action</a>
+            <a className="dropdown-item"
+               href="#">Something else here</a>
+        </div>
+    </div>
 );
 
 DropdownButton.propTypes = {
