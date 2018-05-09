323b39fd7f60a2655c95a0bb6780930df4faa90f
George Higbie <gehigbie@gmail.com>
2018-05-09 16:48:43 +0000

    Add comments

--- src/MyReadsApp.js
+++ src/MyReadsApp.js
@@ -19,8 +19,7 @@ class MyReadsApp extends Component {
     this.componentDidMount = this.componentDidMount.bind(this);    
     
     this.state = {
-      books: [],
-      activeIndex: undefined
+      books: []
     };
   }
   
@@ -33,15 +32,16 @@ class MyReadsApp extends Component {
   }
   
   handleChangeShelf(e){
-    const selectedBook = e.currentTarget.value.split(',');
-    const targetID = selectedBook[0];
-    const targetShelf = selectedBook[1]
-    const targetBook = this.state.books.filter((book) => book.id === targetID);
-    const targetBookIndex = this.state.books.findIndex((book) => book.id === targetID);
+    const selectedBook = e.currentTarget.value.split(','); //This gets the object an turns it into an array
+    const targetID = selectedBook[0]; //This is the value of the ID, which was on the first item on the array
+    const targetShelf = selectedBook[1] //This is the shelf that the book should move to, which was on the second element of the array
+    const targetBook = this.state.books.filter((book) => book.id === targetID); //This is an array with the book that is to be changed
+    const targetBookIndex = this.state.books.findIndex((book) => book.id === targetID); //This is the index of the book that is to changed
     let newBooksArray = this.state.books;
     newBooksArray[targetBookIndex] = targetBook[0];
     this.setState(() => {books: newBooksArray});
     console.log("After first update: ", this.state.books);
+    
     BookAPI.update(targetBook[0], targetShelf)
       .then((response) => {
       console.log(response);
