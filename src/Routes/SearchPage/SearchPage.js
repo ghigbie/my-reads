1763920986cc68983fc01f04c886efed7034934b
George Higbie <gehigbie@gmail.com>
2018-05-12 21:45:59 +0000

    Add propperties to input

--- src/Routes/SearchPage/SearchPage.js
+++ src/Routes/SearchPage/SearchPage.js
@@ -8,9 +8,10 @@ import BookItem  from './../../Components/BookItem/BookItem';
 class SearchPage extends Component{
     constructor(props){
         super(props);
-        
+        this.updateQuery = this.updateQuery.bind(this);
+        this.clearQuery = this.clearQuery.bind(this);
         this.state = {
-            query: ''
+            query: 'react'
         };
     }
     
@@ -18,16 +19,41 @@ class SearchPage extends Component{
         books: PropTypes.array.isRequired
     }
     
+    updateQuery = (query) => {
+        console.log('updateQuery Called');
+        this.setState(() => {{query: query.trim()}});
+    }
+    
+    clearQuery = () => this.updateQuery('')
+    
     render(){
+        const { query } = this.state;
+        const { books } = this.props;
         return(
             <div className="container">
                 This is the search page
                 <BackArrow />
                 <form onSubmit={this.props.searchBooks}>
-                    <input type="text" name="search" />
+                    <input type="text" 
+                           name="search"
+                           placeHolder="Search Books"
+                           value={this.state.query}
+                           onChange={true}/>
                 </form>
                 
                 <div className="book-display search-container">
+                    {this.props.books.filter((book) => book.title === query).map(
+                        (book) => (<BookItem key={book.id}
+                                                          title={book.title}
+                                                          authors={book.authors}
+                                                          image={book.imageLinks.thumbnail}
+                                                          description={book.description}
+                                                          id={book.id}
+                                                          shelf={book.shelf}
+                                                          sectionTitles={this.props.sectionTitles}
+                                                          changeShelf={this.props.changeShelf}/>))}
+                        
+                    
                     {this.props.books.map((book) => (<BookItem key={book.id}
                                                           title={book.title}
                                                           authors={book.authors}
