dadd03430f482cfd11ec42c7c44f2b305b175c85
George Higbie <gehigbie@gmail.com>
2018-05-12 02:40:52 +0000

    Refactor functional component to stateless component

--- src/Routes/SearchPage/SearchPage.js
+++ src/Routes/SearchPage/SearchPage.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './SearchPage.css';
 
 import BackArrow from './../../Components/BackArrow/BackArrow';
 import BookItem  from './../../Components/BookItem/BookItem';
 
-const SearchPage = (props) => (
-    <div className="container">
-        This is the search page
-        <BackArrow />
-        <form onSubmit={props.searchBooks}>
-            <input type="text" name="search" />
-        </form>
+class SearchPage extends Component{
+    constructor(props){
+        super(props);
         
-        <div className="book-display search-container">
-            {props.books.map((book) => (<BookItem key={book.id}
-                                                  title={book.title}
-                                                  authors={book.authors}
-                                                  image={book.imageLinks.thumbnail}
-                                                  description={book.description}
-                                                  id={book.id}
-                                                  shelf={book.shelf}
-                                                  sectionTitles={props.sectionTitles}
-                                                  changeShelf={props.changeShelf}/>))}
-        </div>
-    </div>
-);
+        this.state = {
+            query: ''
+        };
+    }
+    
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        searchBooks: PropTypes.function.isRequired
+    }
+    
+    render(){
+        return(
+            <div className="container">
+                This is the search page
+                <BackArrow />
+                <form onSubmit={this.props.searchBooks}>
+                    <input type="text" name="search" />
+                </form>
+                
+                <div className="book-display search-container">
+                    {this.props.books.map((book) => (<BookItem key={book.id}
+                                                          title={book.title}
+                                                          authors={book.authors}
+                                                          image={book.imageLinks.thumbnail}
+                                                          description={book.description}
+                                                          id={book.id}
+                                                          shelf={book.shelf}
+                                                          sectionTitles={this.props.sectionTitles}
+                                                          changeShelf={this.props.changeShelf}/>))}
+                </div>
+            </div>);
+    }
 
-SearchPage.propTypes = {
-    books: PropTypes.array.isRequired,
-    searchBooks: PropTypes.func.isRequired 
-};
+}
+
 
 export default SearchPage;
