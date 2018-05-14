76d5f03275f2cd224a8e70233cfc8c959d94eba9
George Higbie <gehigbie@gmail.com>
2018-05-14 15:20:23 +0000

    Reformat compoent

--- src/Components/BookItem/BookItem.js
+++ src/Components/BookItem/BookItem.js
@@ -7,30 +7,30 @@ import DropdownButton from './../DropdownButton/DropdownButton';
 import BookModal from './../BookModal/BookModal';
 
 const BookItem = (props) =>
-            (<div className="card book-item">
-                <img className="card-img-top book-image" 
-                     src={this.props.image} 
-                     alt={`the book ${props.title} by ${props.authors.toString().replace(/,/g, ', ')}`} />
-                <div className="card-body">
-                    <h5 className="card-title book-title">{props.title}</h5>
-                    <h5 className="card-title by-line by-line">{`By ${props.authors.toString().replace(/,/g, ', ')}`}</h5>
-                    <p className="card-text book-description">{`${props.description.substring(0, 188)}...`}</p>
-                    <div className="btn-bottom-right">
-                        <DropdownButton sectionTitles={props.sectionTitles}
-                                        id={props.id}
-                                        shelf={props.shelf}
-                                        changeShelf={props.changeShelf}/>
-                    </div>
-                    <div className="btn-bottom">
-                        <BookModal title={props.title}
-                                   authors={props.authors}
-                                   image={props.image}
-                                   description={props.description}
-                                   id={props.id}
-                                   shelf={props.shelf}/>
-                    </div>
-                </div>
-            </div>);
+    (<div className="card book-item">
+        <img className="card-img-top book-image" 
+             src={this.props.image} 
+             alt={`the book ${props.title} by ${props.authors.toString().replace(/,/g, ', ')}`} />
+        <div className="card-body">
+            <h5 className="card-title book-title">{props.title}</h5>
+            <h5 className="card-title by-line by-line">{`By ${props.authors.toString().replace(/,/g, ', ')}`}</h5>
+            <p className="card-text book-description">{`${props.description.substring(0, 188)}...`}</p>
+            <div className="btn-bottom-right">
+                <DropdownButton sectionTitles={props.sectionTitles}
+                                id={props.id}
+                                shelf={props.shelf}
+                                changeShelf={props.changeShelf}/>
+            </div>
+            <div className="btn-bottom">
+                <BookModal title={props.title}
+                           authors={props.authors}
+                           image={props.image}
+                           description={props.description}
+                           id={props.id}
+                           shelf={props.shelf}/>
+            </div>
+        </div>
+    </div>);
 
 
 
