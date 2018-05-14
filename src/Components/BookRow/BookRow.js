96af966cd632dab6a10f87e4414d457c1c4085cc
George Higbie <gehigbie@gmail.com>
2018-05-14 15:26:04 +0000

    Refactor class-based component to functional component

--- src/Components/BookRow/BookRow.js
+++ src/Components/BookRow/BookRow.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './BookRow.css';
 
 import BookItem from './../BookItem/BookItem';
 
-class BookRow extends Component{
-    static propTypes = {
-        heading: PropTypes.string.isRequired,
-        shelfCategory: PropTypes.string.isRequired,
-        books: PropTypes.array.isRequired,
-        sectionTitles: PropTypes.array.isRequired
-    }
-    
-    render(){
-        return(
-            <div>
-                <h4 className="shelf-title">{this.props.heading}</h4>
-                <div className="books-container row">
-                    {this.props.books.filter((book) => 
-                        book.shelf === this.props.shelfCategory).map((book) => {
-                            return (<BookItem key={book.id}
-                                              title={book.title}
-                                              authors={book.authors}
-                                              image={book.imageLinks.thumbnail}
-                                              description={book.description}
-                                              id={book.id}
-                                              shelf={book.shelf}
-                                              sectionTitles={this.props.sectionTitles}
-                                              changeShelf={this.props.changeShelf}/>);
-                    })}
-                </div>
-            </div>
-        );
-    }
-}
+const BookRow = (props) => 
+    (<div>
+        <h4 className="shelf-title">{this.props.heading}</h4>
+        <div className="books-container row">
+            {this.props.books.filter((book) => 
+                book.shelf === this.props.shelfCategory).map((book) => {
+                    return (<BookItem key={book.id}
+                                            title={book.title}
+                                            authors={book.authors}
+                                            image={book.imageLinks.thumbnail}
+                                            description={book.description}
+                                            id={book.id}
+                                            shelf={book.shelf}
+                                            sectionTitles={this.props.sectionTitles}
+                                            changeShelf={this.props.changeShelf}/>);
+            })}
+        </div>
+    </div>
+);
+
+BookRow.propTypes = {
+    heading: PropTypes.string.isRequired,
+    shelfCategory: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    sectionTitles: PropTypes.array.isRequired
+};
 
 export default BookRow;
