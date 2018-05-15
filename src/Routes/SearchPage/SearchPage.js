e8c8f9577fa44a87d1688f6edd7896c0de7ebaea
George Higbie <gehigbie@gmail.com>
2018-05-15 18:23:37 +0000

    Chagne variable names to match functions

--- src/Routes/SearchPage/SearchPage.js
+++ src/Routes/SearchPage/SearchPage.js
@@ -23,8 +23,10 @@ class SearchPage extends Component{
     updateQuery = (query) => {
         this.setState(() => ({query: query}));
         console.log('update query caleld');
-        BookAPI.search(query).then((response) =>
-            (this.setState((prevState) => ({searchedBooks: prevState.searchedBooks.concat(JSON.parse(response))}))));
+        BookAPI.search(this.state.query).then((response) =>
+            (this.setState((prevState) => ({searchedBooks: prevState.searchedBooks.concat(response)}))));
+        console.log(BookAPI.search(query));
+        //this.setState((prevState) => ({searchedBooks: prevState.searchedBooks.concat(BookAPI.search(query))}));
     }
     
     clearQuery = () => this.updateQuery('')
@@ -32,11 +34,11 @@ class SearchPage extends Component{
     render(){
         const { books } = this.props;
         const { query } = this.state;
-        const searchedBooks = query === '' ? [] //books //This varibale filters the list of books. It can either be "books" or "[]", depending on what you want to display
-            : this.state.displayBooks.filter((book) => (  //This funciton filters the books by title and author
+        const filteredBooks = query === '' ? [] //books //This varibale filters the list of books. It can either be "books" or "[]", depending on what you want to display
+            : this.state.searchedBooks.filter((book) => (  //This funciton filters the books by title and author
                 book.title.toLowerCase().includes(query.toLowerCase())) //this line filters by title
                 || book.authors.toString().toLowerCase().includes(query.toLowerCase())); //this line filters by title
-        console.log(searchedBooks);
+        //console.log(searchedBooks);
         return(
             <div className="books-container">
                 <h5 className="search-instructions">Search by Title or by Author</h5>
@@ -49,14 +51,14 @@ class SearchPage extends Component{
                            value={this.state.query}
                            onChange={(event) => this.updateQuery(event.target.value)}/>
                 </form>
-                {searchedBooks.length > 0 &&
+                {filteredBooks.length > 0 &&
                     <div className="showing-books">
                         <span>Now showing {searchedBooks.length} of {books.length}</span>
                         <button onClick={(event) => this.clearQuery()}>Clear Search</button>
                     </div>}
                 
                 <div className="book-display search-display-container">
-                    {searchedBooks.map((book) => (<BookItem key={book.id}
+                    {filteredBooks.map((book) => (<BookItem key={book.id}
                                                             title={book.title}
                                                             authors={book.authors}
                                                             image={book.imageLinks.thumbnail}
