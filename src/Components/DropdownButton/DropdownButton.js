9a841a526d765d26b23e36421f1da83d4415918c
George Higbie <gehigbie@gmail.com>
2018-05-15 14:47:53 +0000

    Add active props to drop down

--- src/Components/DropdownButton/DropdownButton.js
+++ src/Components/DropdownButton/DropdownButton.js
@@ -44,17 +44,17 @@ class DropdownButton extends Component{
           <DropdownItem header>Move to...</DropdownItem>
           <DropdownItem value={`${this.props.id},${this.props.sectionTitles[0].shelfCategory}`}
                         onClick={this.props.changeShelf}
-                        selected={this.isSelected(this.props.sectionTitles[0].shelfCategory)}>
+                        active={this.isSelected(this.props.sectionTitles[0].shelfCategory)}>
                         {this.props.sectionTitles[0].heading}</DropdownItem>
                         
           <DropdownItem value={`${this.props.id},${this.props.sectionTitles[1].shelfCategory}`}
                         onClick={this.props.changeShelf}
-                        selected={this.isSelected(this.props.sectionTitles[1].shelfCategory)}>
+                        active={this.isSelected(this.props.sectionTitles[1].shelfCategory)}>
                         {this.props.sectionTitles[1].heading}</DropdownItem>
                         
           <DropdownItem value={`${this.props.id},${this.props.sectionTitles[2].shelfCategory}`}
                         onClick={this.props.changeShelf}
-                        selected={this.isSelected(this.props.sectionTitles[2].shelfCategory)}>
+                        active={this.isSelected(this.props.sectionTitles[2].shelfCategory)}>
                         {this.props.sectionTitles[2].heading}</DropdownItem>
                         
           <DropdownItem value={`${this.props.id},none`}
